29385fbee73b5be0fc0ad7a87130afee4a738978
Adam Sobotka <adam.sobotka@socialbakers.com>
2022-03-18 23:08:50 +0100

    got it

--- app/routes/add.jsx
+++ app/routes/add.jsx
@@ -1,30 +1,24 @@
 /* eslint-disable no-undef */
-import { json, useLoaderData, redirect, Form } from "remix";
-
-export const loader = async ({ params }) => {
-  // return json(
-  //   await SATNIK.get(`Jelena`, {
-  //     type: "json",
-  //   })
-  //);
-  return json(await SATNIK.get(`Jelena`), { type: "json" });
-};
+import { json, redirect, Form } from "remix";
 
 export const action = async ({ request }) => {
   const formData = await request.formData();
   const id = formData.get("name");
   const description = formData.get("description");
   const age = formData.get("age");
+  const sex = formData.get("sex");
+  const status = "new";
+  const created = Date.now();
   //console.log(title, description);
-  const result = await SATNIK.put(id, JSON.stringify({ description, age }));
+  const result = await SATNIK.put(
+    `${status}:${sex}:${id}`,
+    JSON.stringify({ description, age, status, created })
+  );
   console.log(result);
   return null;
 };
 
-export default function Index() {
-  const hadry = useLoaderData();
-  console.log(hadry);
-
+export default function Add() {
   return (
     <div className="bg-gray-200">
       <h1>Welcome to Satnik</h1>
@@ -46,6 +40,13 @@ export default function Index() {
             Age:
             <br />
             <input name="age" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Sex:
+            <br />
+            <input name="sex" />
           </label>
         </p>
         <p>
